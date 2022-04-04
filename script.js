f20d21046ee4c962d1c4bf6ddf14cfb1f0ecb10f
JulesHeller <julesheller0@gmail.com>
2022-04-04 23:22:57 +0200

    initialisation

---
+++ script.js
@@ -0,0 +1,66 @@
+for (let i = 0; i < 256; i++) {
+    document.querySelector(".container").innerHTML += "<div></div>"
+}
+
+var selectedColour = "blanc"
+let fill = false;
+
+let pixels = document.querySelectorAll(".container div")
+let couleurs = document.querySelectorAll(".couleurs>div:not(.outils)")
+let outils = document.querySelectorAll(".outils div")
+
+pixels.forEach(e => {
+    e.addEventListener("mouseenter", contour)
+    e.addEventListener("mouseleave", contour)
+
+    e.addEventListener("click", fillPixel)
+})
+
+function contour() {
+    this.classList.toggle("contour");
+}
+
+couleurs.forEach(e => {
+    e.addEventListener("click", selectColour)
+})
+
+function selectColour() {
+    couleurs.forEach(e => {
+        e.classList.remove("selected")
+    })
+
+    selectedColour = this.getAttribute("class")
+    console.log(selectedColour)
+
+    this.classList.add("selected")
+}
+
+outils.forEach(e => {
+    e.addEventListener("click", swapTool)
+})
+
+function swapTool() {
+    outils.forEach(e => {
+        e.classList.toggle("cache")
+    })
+
+    if (fill) {
+        fill = false;
+        console.log(fill)
+    } else {
+        fill = true;
+        console.log(fill)
+    }
+}
+
+function fillPixel() {
+    if (fill == true) {
+        pixels.forEach(e=> {
+            e.classList.remove("blanc", "noir", "gris", "rouge", "vert", "bleu", "jaune", "rose")
+            e.classList.add(selectedColour)
+        })
+    } else {
+        this.classList.remove("blanc", "noir", "gris", "rouge", "vert", "bleu", "jaune", "rose")
+        this.classList.add(selectedColour)
+    }
+}
